5accd3736a8e683641ced777f4f2c325b0e6ea48
Chalinator <pgkriss@tlen.pl>
2022-11-23 17:36:23 +0100

    stronicowanie produktów

--- src/main/java/com/ex/shop/product/service/ProductService.java
+++ src/main/java/com/ex/shop/product/service/ProductService.java
@@ -3,19 +3,18 @@ package com.ex.shop.product.service;
 import com.ex.shop.product.model.Product;
 import com.ex.shop.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-    // 38.5 żeby ta metoda zadziałała, trzeba wstrzyknąć repozytorium
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        // 38.6 mogę teraz użyc repozytorium:
-        return productRepository.findAll(); // zwracam wszystkie produkty. Dzięki tej metodzie Hibernate wykona zapytanie, które pobierze listę wszystkich produktów
+    public Page<Product> getProducts(Pageable pageable) { // 5.4 zamieniam na Pageable tak jak w kontrolerze
+        return productRepository.findAll(pageable); // 4.0 dodaję PageRequest z metodą of, strona nr 1 będzie mieć 25 rekordów.
+        // metoda zwraca Page, a nie listę, dlatego jestt bład.
     }
 }
